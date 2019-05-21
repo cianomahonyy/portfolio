3b71bd40790c13967140c90486ccb9e94ae330cb
Cian O Mahony <CIAN@Cians-MacBook-Pro.local>
2019-05-22 00:50:39 +0100

    First Commit

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
+  .get('/coding', (req, res) => res.render('coding'))
+  .get('/design', (req, res) => res.render('design'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
